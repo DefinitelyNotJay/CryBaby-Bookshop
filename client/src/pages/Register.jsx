8a4f8824fb710765ea801d9abca0f3ada20249f4
Tendou26 <jjbrathers3@gmail.com>
2024-07-10 11:32:07 +0700

    create register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,25 @@
 export default function Register() {
   return (
     <div>
+        <h1>ลงทะเบียน</h1>
       <form action="">
         <div>
-          <label htmlFor="">username</label>
-          <input type="text" placeholder="username" />
+          <label htmlFor="">username</label><br/>
+          <input type="text" placeholder="username" name="username" />
         </div>
+        <div>
+          <label htmlFor="">email</label><br/>
+          <input type="email" placeholder="email" name="email"/>
+        </div>
+        <div>
+          <label htmlFor="">password</label><br/>
+          <input type="password" placeholder="password" name="password"/>
+        </div>
+        <div>
+          <label htmlFor="">role</label><br/>
+          <input type="text" placeholder="role" name="role"/>
+        </div>
+        <button>ยืนยัน</button>
       </form>
     </div>
   );
